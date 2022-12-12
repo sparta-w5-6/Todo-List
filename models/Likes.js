da9170566c585ea47238e463f4a047d95f194087
baeksbs3 <baeksbs3@gmail.com>
2022-12-12 18:51:13 +0900

    likes model updated

--- models/Likes.js
+++ models/Likes.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 'use strict';
 const { Model } = require('sequelize');
-=======
-"use strict";
-const { Model } = require("sequelize");
->>>>>>> aaad9181de403a00f693bd6beff862f3987176fc
-=======
-'use strict';
-const { Model } = require('sequelize');
->>>>>>> c3d22248075c2a5b7e7676391b5fb0b78109b4c4
+
 module.exports = (sequelize, DataTypes) => {
   class Likes extends Model {
     /**
@@ -18,18 +9,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-<<<<<<< HEAD
-<<<<<<< HEAD
       this.belongsTo(models.Users, { foreignKey: 'userId' });
       this.belongsTo(models.Todos, { foreignKey: 'todoId' });
-=======
-      this.belongsTo(models.Users, { foreignKey: "userId" });
-      this.belongsTo(models.Todos, { foreignKey: "todoId" });
->>>>>>> aaad9181de403a00f693bd6beff862f3987176fc
-=======
-      this.belongsTo(models.Users, { foreignKey: 'userId' });
-      this.belongsTo(models.Todos, { foreignKey: 'todoId' });
->>>>>>> c3d22248075c2a5b7e7676391b5fb0b78109b4c4
     }
   }
   Likes.init(
@@ -42,40 +23,20 @@ module.exports = (sequelize, DataTypes) => {
       },
       todoId: {
         type: DataTypes.INTEGER,
-<<<<<<< HEAD
-<<<<<<< HEAD
         references: { model: 'Todos', key: 'todoId' },
         onDelete: 'CASCADE',
-=======
-        references: { model: "Todos", key: "todoId" },
-        onDelete: "CASCADE",
->>>>>>> aaad9181de403a00f693bd6beff862f3987176fc
-=======
-        references: { model: 'Todos', key: 'todoId' },
-        onDelete: 'CASCADE',
->>>>>>> c3d22248075c2a5b7e7676391b5fb0b78109b4c4
       },
 
       userId: {
         type: DataTypes.INTEGER,
-<<<<<<< HEAD
-<<<<<<< HEAD
         references: { model: 'Users', key: 'userId' },
         onDelete: 'CASCADE',
       },
 
-=======
-        references: { model: "Users", key: "userId" },
-        onDelete: "CASCADE",
-=======
-        references: { model: 'Users', key: 'userId' },
-        onDelete: 'CASCADE',
->>>>>>> c3d22248075c2a5b7e7676391b5fb0b78109b4c4
-      },
       isLike: {
         type: DataTypes.BOOLEAN,
       },
->>>>>>> aaad9181de403a00f693bd6beff862f3987176fc
+
       createdAt: {
         allowNull: false,
         type: DataTypes.DATE,
@@ -87,17 +48,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-<<<<<<< HEAD
-<<<<<<< HEAD
-      modelName: 'Likes',
-=======
-      modelName: "Likes",
->>>>>>> aaad9181de403a00f693bd6beff862f3987176fc
-    }
-=======
       modelName: 'Likes',
     },
->>>>>>> c3d22248075c2a5b7e7676391b5fb0b78109b4c4
   );
   return Likes;
 };
