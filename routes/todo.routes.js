3f5259f8b68fe4f708de4f5343c8a4e625114958
baeksbs3 <baeksbs3@gmail.com>
2022-12-12 23:30:54 +0900

    authmiddleware added

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { Todos } = require('../models');
 const router = express.Router();
 const TodoController = require('../controllers/todo.controller');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const todoController = new TodoController();
 
-router.post('/', todoController.createTodo);
+router.post('/', authMiddleware, todoController.createTodo);
+router.put('/:todoId', authMiddleware, todoController.updateTodo);
 module.exports = router;
--- services/todo.service.js
+++ services/todo.service.js
@@ -19,6 +19,27 @@ class TodoService {
     });
     return todo;
   };
+  updateTodo = async (todoId, title, item, userId) => {
+    //todo 게시글이 존재하지 않을 경우 에러 처리
+    // const todoExists = await this.TodoRepository.findOne(todoId);
+    // console.log('todoExists:', todoExists);
+    // if (!todoExists) {
+    //   throw new doesntExistError('todo게시글이 존재하지 않습니다');
+    // }
+    //title, item 미입력시 에러 처리
+    // if (!title || !item) {
+    //   throw new Error('입력 값이 올바르지 않습니다');
+    // }
+    const update = await this.TodoRepository.updateTodo(
+      todoId,
+      title,
+      userId,
+      item,
+    );
+    console.log('update Index: ', update[0]);
+
+    return update;
+  };
 }
 
 module.exports = TodoService;
