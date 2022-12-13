096186e5483be0288d5dac3d63544bf0f7a30bd1
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2022-12-13 13:59:57 +0900

    test code 수정 Users

--- __test__/unit/repositories/login.repository.spec.js
+++ __test__/unit/repositories/login.repository.spec.js
@@ -1,21 +1,20 @@
-const { default: JestHasteMap } = require('jest-haste-map');
-const { beforeEach } = require('node:test');
-const { describe } = require('yargs');
 const LoginRepository = require('../../../repositories/login.repository');
 
 const { findOneUser } = require('../../fixtures/login.fixtures');
 
-const mockLoginModel = () => ({ findOne: JestHasteMap.fn() });
+const mockLoginModel = () => ({ findOne: jest.fn() });
 
 describe('Login Repository layer test', () => {
   let loginRepository = new LoginRepository();
   loginRepository.Users = mockLoginModel();
 
   beforeEach(() => {
-    JestHasteMap.resetAllMocks();
+    jest.resetAllMocks();
   });
 
   test('findOne Methods toHaveBeenCalled', async () => {
-    const users = await loginRepository.findOne;
+    const users = await loginRepository.findOne();
+
+    expect(loginRepository.Users.find).toHaveBeenCalledTimes(1);
   });
 });
