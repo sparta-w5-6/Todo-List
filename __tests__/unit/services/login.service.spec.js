ad95708d9df232e1bcbe577aa5523e863523233c
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2022-12-14 15:14:24 +0900

    login Service 테스트 진행중

--- __tests__/unit/services/login.service.spec.js
+++ __tests__/unit/services/login.service.spec.js
@@ -1,5 +1,6 @@
 const LoginService = require('../../../services/login.service');
-import jwt from jsonwebtoken
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 // findUser 메소드 모킹
 let mockLoginRepository = {
   findUser: jest.fn(),
@@ -7,6 +8,8 @@ let mockLoginRepository = {
   sign: jest.fn(),
 };
 jest.mock('jsonwebtoken');
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 // loginService를 생성자로 생성
 const loginService = new LoginService(mockLoginRepository);
@@ -43,16 +46,18 @@ describe('Login service test', () => {
     expect(mockLoginRepository.findUser).toHaveBeenCalledTimes(1);
   });
 
-
   // FIXME : 이거 어케 함
   // 1. login 메소드는 파라미터로 받아온 값들이 DB User 테이블에 있는 데이터(mock 데이터)와 일치하는지
   // 2. 일치하다면 정상적으로 토큰이 생성되는지
 
-//   test('login Service sign Method', async () => {
-//     const loginReturnValue = {
-//       token:
-//         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoidGVzdEB0ZXN0LmNvbSIsIm5pY2tuYW1lIjoidGVzdCIsImlhdCI6MTY3MDk0OTYzMn0.bmeCk1wEVJBqmzfCXOCH9y85aPDZqC_xthLIWyOgaoU',
-//     };
-//     const loginBodyParams = { email: 'test@example.com', password: '1234' };
-//   });
+  test('login Service sign Method', async () => {
+    const token = jwt.sign(
+      { userId: 1, email: 'test@example.com' },
+      SECRET_KEY,
+      { expireIn: '1m', issuer: 'najiwon' },
+    );
+    const req = { headers: { authorization: token } };
+    verifyToken(req, res, next);
+    expect(next).toHaveBeenCalledTimes(1);
+  });
 });
