e7486f63681ebe41514495e3546a698f23e5b6a7
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2022-12-14 23:45:09 +0900

    User controller, service, repository test complete

--- __tests__/unit/controllers/login.controller.spec.js
+++ __tests__/unit/controllers/login.controller.spec.js
@@ -1,6 +1,4 @@
 const LoginController = require('../../../controllers/login.controller');
-const jwt = require('jsonwebtoken');
-const SECRET_KEY = process.env.SECRET_KEY;
 
 let mockLoginService = {
   loginUser: jest.fn(),
@@ -11,25 +9,26 @@ let mockLoginService = {
 let mockRequest = { body: jest.fn() };
 let mockResponse = { status: jest.fn(), json: jest.fn() };
 
-loginController.loginService = mockLoginService;
+// loginController.loginService = mockLoginService;
 
 describe('LoginController', () => {
   beforeEach(() => {
     jest.resetAllMocks();
-    mockResponse.status = jest(() => mockResponse);
+    mockResponse.status = jest.fn(() => mockResponse);
   });
 
+  // 로그인을 통해 토큰이 잘 생성되는지 테스트
+  // body로 들어오는 email 과 password 가 db에 저장된 유저의 정보와 일치하다면(findUser)
+  // service의 login 메소드를 통해 토큰을 생성하고, 그 쿠키를 response 한다.(login)
   test('login Controller login Method create token', async () => {
     const loginBodyParams = {
       email: 'test@test.com',
       password: '1234',
     };
-
-    const tokenReturnValue = await loginController.loginService.login({
+    const token = mockLoginService.login({
       email: loginBodyParams.email,
       password: loginBodyParams.password,
     });
-    const token = mockResponse.cookie('token', token);
-    expect(token).toEqual(token);
+    expect(mockLoginService.login(loginBodyParams)).toEqual(token);
   });
 });
---
+++ __tests__/unit/middlewares/authMiddleware.spec.js
@@ -0,0 +1,5 @@
+// type: value 형태로 토큰이 없을 때 에러 throw / 있을 때
+
+//  verify 유효하지 않은 토큰일 때 / 유효할 때
+
+// userId -- findByPk 성공 / 실패
