b237f6d8696bb12ce62b2b0f46d0bc9eeb58818a
itsjustpeter <develop@rhinobyte.io>
2022-12-14 22:05:26 +0900

    test: fix test configuration for testing with TypeScript codes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // TODO 테스트까지 TypeScript 마이그레이션 이후 /__tests__ 사용
+  rootDir: 'dist/__tests__',
   // 해당 패턴에 일치하는 경로가 존재할 경우 테스트를 하지 않고 넘어갑니다.
   testPathIgnorePatterns: ['/node_modules/(?!my-package)(.*)'],
   // 테스트 실행 시 각 TestCase에 대한 출력을 해줍니다.
